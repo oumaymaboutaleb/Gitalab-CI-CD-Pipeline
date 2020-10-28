5f3a06350f2e9da4aa83e7068c4cd9e2df8fbd48
nikitsrj <swarajnikit@gmail.com>
2020-10-29 02:22:43 +0800

    adding test cases

---
+++ test/student.js
@@ -0,0 +1,80 @@
+process.env.NODE_ENV = "test"
+
+const Student = require('../models').Student;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const server = require('../server');
+const should = chai.should();
+
+chai.use(chaiHttp)
+
+
+describe('/GET students', () => {
+    it('it should Get all students', (done) => {
+        chai.request(app)
+        .get('/students')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            done();
+        });
+    });
+});
+
+describe('/POST students', () => {
+    it('it sould post the student info', (done) => {
+        const student = {
+            firstname: "Tony",
+            lastname: "Stark",
+            class: "PHD",
+            nationality: "USA"
+        };
+
+        chai.request(app)
+        .post('/students')
+        .send(student)
+        .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message').eql('Post Created successfully');
+            done();
+        });
+    });
+});
+
+
+describe('/PUT/:id student', () => {
+    it("should update the student info", (done) => {
+        const student = {
+            firstname: "lol",
+            lastname: "Himu",
+        }
+        const Id = 22420;
+         chai.request(app)
+         .put('/students/' + Id)
+         .send(student)
+         .end((err, res) => {
+             res.should.have.status(200);
+             res.body.should.be.a('object');
+             res.body.should.have.property('message').eql('Post updated successfully');
+             done();
+         });
+    });
+});
+
+describe('/DELETE/:id student', () => {
+    it('it should DELETE the student recored given the id', (done) => {
+        const student = {
+            "id": 22830
+        }
+         chai.request(app)
+         .delete('/students/' + student.id)
+         .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Post deleted successfully');
+                done();
+         });
+    });
+});
